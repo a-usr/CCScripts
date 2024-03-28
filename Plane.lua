ff73a1a7209a48818576dabc2f20a597b87d48c3
John Züchler <johnzuchler@gmail.com>
2024-03-28 15:58:58 +0100

    plane script update

--- Plane.lua
+++ Plane.lua
@@ -1,57 +1,43 @@
 local front = peripheral.wrap("front")
 local back = peripheral.wrap("back")
-local monitors = {peripheral.find("monitor") }
 local FrLprev = 0
 local FrRprev = 0
 local BLprev = 0
 local BRprev = 0
 while true do
-    local DTilt = front.getAnalogInput("front")
-    local UTilt = back.getAnalogInput("front")
-    local LTwist = front.getAnalogInput("top")
-    local RTwist = back.getAnalogInput("top")
-    --[[for _, monitor in pairs(monitors) do
-        monitor.setCursorPos(0, 0)
-        monitor.write("DTilt: ")
-        monitor.write(DTilt)
-        monitor.setCursorPos(0, 1)
-        monitor.write("UTilt: ")
-        monitor.write(UTilt)
-        monitor.setCursorPos(0, 2)
-        monitor.write("LTwist: ")
-        monitor.write(LTwist)
-        monitor.setCursorPos(0, 3)
-        monitor.write("RTwist: ")
-        monitor.write(RTwist)
-        monitor.setCursorPos(0, 0)
-    end 
-    print("DTilt: ")
-    print(DTilt)
-    print("UTilt: ")
-    print(UTilt)
-    print("LTwist: ")
-    print(LTwist)
-    print("RTwist: ")
-    print(RTwist)]]--
+    local DTilt, UTilt, LTwist, RTwist
+    local f1 = function() DTilt = front.getAnalogInput("front") end
+    local f2 = function() UTilt = back.getAnalogInput("front") end
+    local f3 = function() LTwist = front.getAnalogInput("top") end
+    local f4 = function() RTwist = back.getAnalogInput("top") end
+    parallel.waitForAll(f1,f2,f3,f4)
     FrL = (DTilt-LTwist)+(RTwist-UTilt)
     FrR = (DTilt-RTwist)+(LTwist-UTilt)
     BL = (UTilt-LTwist)+(RTwist-DTilt)
     BR = (UTilt-RTwist)+(LTwist-DTilt)
-    if not (FrL == FrLprev) then
-        FrLprev = FrL
-        front.setAnalogOutput("left", FrL)
+    f1 = function()
+        if not (FrL == FrLprev) then
+            FrLprev = FrL
+            front.setAnalogOutput("left", FrL)
+        end
     end
-    if not (FrR == FrRprev) then
-        FrRprev = FrR
-        front.setAnalogOutput("right", FrR)
+    f2 = function()
+        if not (FrR == FrRprev) then
+            FrRprev = FrR
+            front.setAnalogOutput("right", FrR)
+        end
     end
-    if not (BL == BLprev) then
-        BLprev = BL
-        back.setAnalogOutput("left", BL)
+    f3 = function()
+        if not (BL == BLprev) then
+            BLprev = BL
+            back.setAnalogOutput("left", BL)
+        end
     end
-    if not( BR == BRprev) then
-        BRprev = BR
-        back.setAnalogOutput("right", BR)
+    f4 = function()
+        if not( BR == BRprev) then
+            BRprev = BR
+            back.setAnalogOutput("right", BR)
+        end
     end
-    -- os.pullEvent("redstone") --
+    parallel.waitForAll(f1,f2,f3,f4)
 end
